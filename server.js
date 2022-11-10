c582a480ecadb86a9ab938f85f0ab2e1d2e1aa92
DESKTOP-11USOID\acer <abkkd5524@gmail.com>
2022-11-10 17:53:14 +0800

    Make change deploy

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const courseRoute = require("./routes").course;
 const passport = require("passport");
 require("./config/passport")(passport);
 const cors = require("cors");
+const path = require("path");
+const port = process.env.PORT || 8080; //process.env.PORT是Heroku自動動態設定
 
 //連接mongooseDB
 mongoose
@@ -23,17 +25,32 @@ mongoose
     console.log(e);
   });
 
+//middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")));
+
 app.use("/api/user", authRoute);
+//course route應該被jwt保護
+//如果request header內部沒有jwt，則request就會被視為unauthorized
 app.use(
   "/api/courses",
   passport.authenticate("jwt", { session: false }),
   courseRoute
 );
+
 app.use("/api/email", emailRoute);
 
-app.listen(8080, () => {
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => {
   console.log("Server running on part 8080.");
 });
